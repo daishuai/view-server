0391c06f883a845a0edb2aa53e30c7cfd59dcba3
daishuai <daishuai@kedacom.com>
2022-11-15 09:31:04 +0800

    脚本管理

---
+++ view-server/script/create_and_init.sql
@@ -0,0 +1,113 @@
+-- ----------------------------
+-- Table structure for resource_info
+-- ----------------------------
+DROP TABLE IF EXISTS `resource_info`;
+CREATE TABLE `resource_info`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '资源标识',
+  `path` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '路由',
+  `icon` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '图标',
+  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '地址',
+  `parent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '父级资源',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名称',
+  `hide_in_bread` bit(1) NULL DEFAULT NULL,
+  `hide_in_menu` bit(1) NULL DEFAULT NULL,
+  `status` bit(1) NULL DEFAULT NULL COMMENT '有效性',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `created_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `updated_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '更新人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of resource_info
+-- ----------------------------
+INSERT INTO `resource_info` VALUES (1, 'doc', '', 'ios-book', NULL, '-1', '文档', b'0', b'0', b'1', '2022-11-14 13:12:52', 'admin', '2022-11-14 13:13:01', 'admin');
+
+-- ----------------------------
+-- Table structure for role_info
+-- ----------------------------
+DROP TABLE IF EXISTS `role_info`;
+CREATE TABLE `role_info`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '角色名',
+  `sign` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '角色标识',
+  `sort` int(11) NULL DEFAULT NULL COMMENT '显示顺序',
+  `status` bit(1) NULL DEFAULT NULL COMMENT '有效性',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `created_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `updated_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '更新人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '角色信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role_info
+-- ----------------------------
+INSERT INTO `role_info` VALUES (1, '超级管理员', 'super-admin', 1, b'1', '2022-11-14 13:10:20', 'admin', '2022-11-14 13:10:24', 'admin');
+
+-- ----------------------------
+-- Table structure for role_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `role_resource`;
+CREATE TABLE `role_resource`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `resource_id` int(11) NULL DEFAULT NULL COMMENT '资源id',
+  `role_sign` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '角色标识',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `created_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `updated_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '更新人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role_resource
+-- ----------------------------
+INSERT INTO `role_resource` VALUES (1, 1, 'super-admin', '2022-11-14 13:13:41', 'admin', '2022-11-14 13:13:43', 'admin');
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '密码',
+  `birthday` date NULL DEFAULT NULL COMMENT '出生日期',
+  `phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '手机号',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '邮箱',
+  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '地址',
+  `status` bit(1) NULL DEFAULT NULL COMMENT '有效性',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `created_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `updated_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '更新人',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES (1, 'super_admin', '123456', '2022-11-14', '123213131', NULL, NULL, NULL, '2022-11-14 09:52:41', 'admin', '2022-11-14 09:52:43', 'admin');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` int(11) NULL DEFAULT NULL COMMENT 'id',
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '用户名',
+  `role_sign` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '角色标识',
+  `created_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `created_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人',
+  `updated_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `updated_by` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '更新人'
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (NULL, 'super_admin', 'super-admin', '2022-11-14 13:10:50', 'admin', '2022-11-14 13:10:53', 'admin');
+
